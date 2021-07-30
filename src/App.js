d1e14603d81ab4767082ca107d937a25e2b0c04a
Giovanny <giovannyramirezs@hotmail.com>
2021-07-29 20:14:32 -0500

    Connect Context into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 import Characters from './components/Characters';
 import Header from './components/Header';
+import ThemeContext from './context/ThemeContext';
 
 function App() {
+
+  const [theme, setTheme] = useState("LightMode");
+
   return (
-    <div className="App">
-      <Header />
-      <Characters />
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={"App " + theme}>
+        <Header />
+        <Characters />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
